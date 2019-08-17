dc0214c46a5cb643c2f1f5af63f46312a459bfcc
jaeryo2357 <jaeryo2357@naver.com>
2019-08-17 16:37:37 +0900

    Doit_HelloKotiln

---
+++ Project/src/Properties.kt
@@ -0,0 +1,12 @@
+//자바에서는 지원하지 않음 그래서 getter setter를 지정해줘야함
+
+//var(mutable) val(read-only)
+class Address{
+
+    var name:String="Kotlin"
+    val city:String="Seoul"
+}
+
+//var <propertyName>[:<PropertyType>][=<property_initializer>]
+//[<getter>]
+//[<setter>]
